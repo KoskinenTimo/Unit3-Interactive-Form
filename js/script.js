3c16aca77faaef9a59721682e863e0d253595ba4
Timo Koskinen <koskinentht@gmail.com>
2021-03-01 13:52:09 +0200

    comments added to script.js and most event listeners

--- js/script.js
+++ js/script.js
@@ -1,21 +1,26 @@
+// Focus starts from name Input
 const nameInput = document.getElementById("name");
 nameInput.focus();
 
+// Job Role and Other Job Role Input variables
 const selectJobRole = document.getElementById("title");
 const otherJobInput = document.getElementById("other-job-role");
 otherJobInput.style.display = "none";
 
-selectJobRole.addEventListener('change', function(e) {
+// Other Job Role Input visible only when chosen from Job Role
+selectJobRole.addEventListener('change', function(e){
   if (e.target.value === 'other') {
     otherJobInput.style.display = "";
   }
 });
 
+// Design and Color selector variables
 const selectDesign = document.getElementById("design");
 const selectColor = document.getElementById("color");
 selectColor.disabled = true;
-console.log(selectColor.children);
 
+// Colors dedicated to specific Design visible when Design selected
+// Color options disabled until a Design pick made
 selectDesign.addEventListener('change', function(e){
   selectColor.disabled = false;
   const designPick = e.target.value;
@@ -28,3 +33,22 @@ selectDesign.addEventListener('change', function(e){
     }
   });
 });
+
+// Variables for Activities fieldset and Total Cost element/counting
+const activitiesField = document.getElementById("activities");
+const totalCostElement = document.getElementById("activities-cost");
+let totalCost = 0;
+
+// Summing to 'totalCost' variable the CHECKED items' costs from Activities and 
+// UNCHECKED items are reduced. 'totalCostElement' updated after each 'change'.
+activitiesField.addEventListener('change', function(e){
+  const item = e.target;
+  const itemCost = parseInt(item.getAttribute('data-cost'));
+  if (item.checked === true) {
+    totalCost += itemCost;
+  } else {
+    totalCost -= itemCost
+  }
+  totalCostElement.innerHTML = `Total: $${totalCost}`
+});
+
